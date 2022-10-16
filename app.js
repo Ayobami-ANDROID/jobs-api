95d3880caa81a741e7edfae3070d41022dcd2a5a
Ayobami-ANDROID <79742255+Ayobami-ANDROID@users.noreply.github.com>
2022-10-17 00:22:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,16 +20,17 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy',1)
-app.use(rateLimiter({
-  windowMs:15*60*1000,
-  max:100,
-}))
+
 app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(xss())
+app.use(rateLimiter({
+  windowMs:15*60*1000,
+  max:100,
+}))
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res) => {
   res.send('job api')
 })
 
